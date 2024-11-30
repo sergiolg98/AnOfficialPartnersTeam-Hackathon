ba529b0e698c160f5bef9a4de34e99bd4f952fbb
Sergio Laureano <sergiolaureano98@gmail.com>
2024-11-29 22:10:44 -0500

    fix: eslint temporary fix

--- src/app/api/developers/route.ts
+++ src/app/api/developers/route.ts
@@ -1,4 +1,5 @@
 export async function POST(request: Request) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const res = await request.json();
     return Response.json({ res })
 }
