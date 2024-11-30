6217debe9aa545aff93eb3dc43b3e8dae06491b6
George Maxi <31070682+Brianmax@users.noreply.github.com>
2024-11-30 09:44:45 -0500

    fix: resolve merge conflicts

--- src/server/api/routers/client.ts
+++ src/server/api/routers/client.ts
@@ -1,17 +1,37 @@
-import { clearLine } from "readline";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const clientsRouter = createTRPCRouter({
-  getById: publicProcedure
+    getById: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(async ({ ctx, input }) => {
       const client = await ctx.db.client.findUnique({
         where: { id: input.id },
+        select: {
+          id: true,
+          name: true,
+          projects: {
+            select: {
+              id: true,
+              name: true,
+              projectDescription: true,
+              status: true,
+              techStack: {
+                select: {
+                  technicalSkill: {
+                    select: {
+                      name: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
       });
 
       if (!client) {
-        throw new Error("Developer not found");
+        throw new Error("Client not found");
       }
 
       return client;
