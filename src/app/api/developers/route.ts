55e05cc45e6edf3ec37749dfd7829a7443ce64cc
Sergio Laureano <sergiolaureano98@gmail.com>
2024-11-29 22:36:29 -0500

    feat: write to DB

--- src/app/api/developers/route.ts
+++ src/app/api/developers/route.ts
@@ -1,4 +1,11 @@
+import { db } from "~/server/db";
+
 export async function POST(request: Request) {
     const res = await request.json();
+    await db.post.create({
+        data: {
+            name: JSON.stringify(res),
+        }
+    })
     return Response.json({ res })
 }
