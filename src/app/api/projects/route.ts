95c37369b265868f6e21d4b93a8e53f6c7866b67
George Maxi <31070682+Brianmax@users.noreply.github.com>
2024-11-30 08:27:40 -0500

    feat: create post method for project model

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,19 +1,76 @@
 import { db } from "~/server/db";
+import { NextRequest } from 'next/server';
+import { Project } from "@prisma/client";
 
-export async function GET() {
-    const projects = await db.project.findMany({
-        include: {
-            techStack: {
-                include: {
-                    technicalSkill: true,
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const projectId = searchParams.get('id');
+
+    if (projectId) {
+        const project = await db.project.findUnique({
+            where: { id: Number(projectId) },
+            include: {
+                techStack: {
+                    include: {
+                        technicalSkill: true,
+                    },
                 },
+                client: true,
+                position: true,
             },
-            client: true,
-            position: true,
-        },
-    });
+        });
 
-    return new Response(JSON.stringify(projects), {
-        headers: { "Content-Type": "application/json" },
-    });
+        return new Response(JSON.stringify(project), {
+            headers: { "Content-Type": "application/json" },
+        });
+    } else {
+        const projects = await db.project.findMany({
+            include: {
+                techStack: {
+                    include: {
+                        technicalSkill: true,
+                    },
+                },
+                client: true,
+                position: true,
+            },
+        });
+
+        return new Response(JSON.stringify(projects), {
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const { name, projectDescription, techStack, clientId, positionId } = await request.json();
+
+        const newProject = await db.project.create({
+            data: {
+                name,
+                projectDescription,
+                status: 'PENDING',
+                techStack: {
+                    create: techStack.map((skill: { technicalSkillId: number }) => ({
+                        technicalSkillId: skill.technicalSkillId
+                    }))
+                },
+                client: clientId ? { connect: { id: clientId } } : undefined,
+                position: positionId ? { connect: { id: positionId } } : undefined,
+            },
+        });
+
+        return new Response(JSON.stringify(newProject), {
+            status: 201, 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response('Error al crear el proyecto', {
+            status: 500,
+        });
+    }
 }
