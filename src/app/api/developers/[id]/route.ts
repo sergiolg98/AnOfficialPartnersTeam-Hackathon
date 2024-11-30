43cf5221ffc3b416b08c675fda61539109c13e5f
Sergio Laureano <sergiolaureano98@gmail.com>
2024-11-29 21:38:30 -0500

    feat: add GET URL first progress

---
+++ src/app/api/developers/[id]/route.ts
@@ -0,0 +1,8 @@
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+  ) {
+    
+    const slug = (await params).id // 'a', 'b', or 'c'
+    return Response.json({message: slug});
+  }
