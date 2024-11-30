1486b1ed0066384d23c9c543d0048585c31f202d
Rafael David <rafael.david@ucsp.edu.pe>
2024-11-30 00:47:27 -0500

    feat: added matching and new-client routes.

--- src/app/matching/page.tsx
+++ src/app/matching/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
+import PaymentForm from "../_components/form";
 
-import PaymentForm from "./_components/form";
 export default function Page() {
   return (
     <main className="bg-background dark flex min-h-screen items-center justify-center">
